2c774d6514978545f622e93be83909c0530166e4
chaudhari sunny <chaudharisunny6@gmail.com>
2025-03-09 15:55:22 +0530

    chang dbs and env

--- config/db.js
+++ config/db.js
@@ -8,7 +8,10 @@ const pool=new pg.Pool({
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    host:process.env.DB_HOST
+    host:process.env.DB_HOST,
+    connectionString: process.env.DB_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+
 })
 
 // pool.query('SELECT current_database(), current_user')
